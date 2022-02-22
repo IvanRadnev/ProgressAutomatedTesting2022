c6b0abf7a20962e6859643553b359304265ad4c0
Ivan <ivan.radnev@sportal.bg>
2022-02-22 22:08:19 +0200

    Homework

--- Lecture 1/src/com/company/Homework.java
+++ Lecture 1/src/com/company/Homework.java
@@ -1,2 +1,17 @@
-package com.company;public class Homework {
+package com.company;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Homework {
+
+    public static void main(String[] args) {
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 10 + 1);
+
+        System.out.println("Generated number is: " + randomNum);
+
+        if (randomNum > 5){
+            System.out.println("The number is greater than 5.");
+        }
+
+    }
 }
--- Lecture 1/src/com/company/Main.java
+++ Lecture 1/src/com/company/Main.java
@@ -5,5 +5,17 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Java");
+
+        String name = "Ivan";
+        int number = 5;
+        double price = 2.64;
+        boolean bool = true;
+
+        System.out.println(name);
+        System.out.println(number);
+        System.out.println(price);
+        System.out.println(bool);
+
+
     }
 }
