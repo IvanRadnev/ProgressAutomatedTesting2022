d853fbf827925a3e9de8aaecb91986ec22eb6ca2
Ivan <ivan.radnev@sportal.bg>
2022-02-25 12:52:21 +0200

    hw1 if statement

--- Lecture 2/src/com/company/Homework1.java
+++ Lecture 2/src/com/company/Homework1.java
@@ -12,6 +12,14 @@ public class Homework1 {
         num = scan.nextInt();
         scan.close();
 
+        //The easy way, with "Math" class.
         System.out.println("Absolute Value of the number is: " + Math.abs(num));
+
+        //With if statement.
+        if (num < 0){
+            System.out.println("Absolute Value of the number is: " + num * -1);
+        } else {
+            System.out.println("Absolute Value of the number is: " + num);
+        }
     }
 }
