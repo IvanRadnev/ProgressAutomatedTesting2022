2a52b0db7f4c765323ee6666596ac8f579f2be39
Ivan <ivan.radnev@sportal.bg>
2022-02-26 21:39:07 +0200

    Homework 2

--- Lecture 2/src/com/company/Homework2.java
+++ Lecture 2/src/com/company/Homework2.java
@@ -1,7 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Homework2 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter a number of classes held:");
+        int classesHeld = input.nextInt();
+
+        System.out.println("Enter a number of classes attended:");
+        int classesAttended = input.nextInt();
+
+        input.close();
+
+        int percentageOfClassAttended = (classesAttended * 100) / classesHeld;
+
+        if (percentageOfClassAttended >= 75){
+            System.out.println(percentageOfClassAttended + "%\nYou are able to attend.");
+        } else {
+            System.out.println(percentageOfClassAttended + "%\nYou are not able to attend.");
+        }
     }
 }
