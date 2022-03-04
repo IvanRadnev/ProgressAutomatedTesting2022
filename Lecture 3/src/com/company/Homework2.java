4eb648d080b494540029bd5fd80e343bd61abe02
Ivan <ivan.radnev@sportal.bg>
2022-03-04 17:09:12 +0200

    Homework 2

---
+++ Lecture 3/src/com/company/Homework2.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = input.nextInt();
+
+        int fact = 1;
+
+        for (int i = 1; i <= number; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial of " + number + " is: " + fact);
+    }
+}
