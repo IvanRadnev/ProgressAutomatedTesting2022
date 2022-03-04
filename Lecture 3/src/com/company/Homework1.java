2d08e01d9cb697ab212405ac179d61bf49f00f76
Ivan <ivan.radnev@sportal.bg>
2022-03-04 17:01:50 +0200

    Homework 1

---
+++ Lecture 3/src/com/company/Homework1.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many numbers will you enter: ");
+        int numbers = input.nextInt();
+
+        System.out.println("Enter the numbers: ");
+
+        int minNum = Integer.MAX_VALUE;
+        int maxNum = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= numbers; i++) {
+            int num = input.nextInt();
+
+            if (num < minNum){
+                minNum = num;
+            }
+            if (num > maxNum){
+                maxNum = num;
+            }
+        }
+
+        input.close();
+
+        System.out.println("Min = " + minNum);
+        System.out.println("Max = " + maxNum);
+    }
+}
---
+++ Lecture 3/src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Scanner input = new Scanner(System.in);
+//        int n=1;
+//        System.out.println("Add number: ");
+//        int inputN = input.nextInt();
+//        input.close();
+//
+//        while ( n<=inputN ){
+//            System.out.println("Next number: " + n++);
+//        }
+
+//        int num = 123, reversed = 0;
+//
+//        System.out.println("Original Number: " + num);
+//
+//        // run loop until num becomes 0
+//        while(num != 0) {
+//
+//            // get last digit from num
+//            int digit = num % 10;
+//            reversed = reversed * 10 + digit;
+//
+//            // remove the last digit from num
+//            num /= 10;
+//        }
+//
+//        System.out.println("Reversed Number: " + reversed);
+
+
+
+//        int num = 123, reversed = 0;
+//
+//        for(;num != 0; num /= 10) {
+//            int digit = num % 10;
+//            reversed = reversed * 10 + digit;
+//        }
+//
+//        System.out.println("Reversed Number: " + reversed);
+
+
+
+
+
+
+
+    }
+}
+
+
+
