b0b2522b9169781d9db00854659bb363ad702199
Ivan <ivan.radnev@sportal.bg>
2022-03-14 13:05:02 +0200

    Homework 2

--- Lecture 5/src/com/company/Homework2.java
+++ Lecture 5/src/com/company/Homework2.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Homework2 {
     public static void main(String[] args) {
 
+        System.out.println(randomNum());
+    }
+
+    public static String randomNum() {
+        Random r = new Random();
+        int arr[] = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt((10 - 1) + 1) + 1;
+
+        }
+        return Arrays.toString(arr);
     }
 
 }
