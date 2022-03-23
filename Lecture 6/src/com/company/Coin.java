5427aa385322596ed088ef0cdc91544689fa6f3f
Ivan <ivan.radnev@sportal.bg>
2022-03-23 11:46:43 +0200

    Coin flip

--- Lecture 6/src/com/company/Coin.java
+++ Lecture 6/src/com/company/Coin.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Random;
+
 public class Coin {
 
-    public String flip(){
-        return "flip";
+    Random randomNum = new Random();
+
+    public String flip() {
+        int result = randomNum.nextInt(2);
+        if (result == 0) {
+            return "You flipped Heads!";
+        }
+        return "You flipped Tails!";
     }
 }
