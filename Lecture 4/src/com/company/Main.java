40baf07566e5a4eb6fd893fb32c2b30421004b8b
Ivan <ivan.radnev@sportal.bg>
2022-03-29 09:53:15 +0300

    conflicts

---
+++ Lecture 4/src/com/company/Main.java
@@ -0,0 +1,110 @@
+package com.company;
+
+import java.security.PublicKey;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        int arr[] = {11, 72, 33, 54, 35 , 6};
+        int arrLength = arr.length;
+
+        for (int i = 0; i < arrLength; i++) {
+//            System.out.println(arr[i]);
+            if (arr[i] == 33){
+                System.out.println(i);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+//        //ex.1
+////        String exampleString = new String("Example text");
+////
+////        System.out.println("Length: " + exampleString.length());
+////
+////        for (int i = 0; i < exampleString.length(); i++) {
+////            System.out.println(exampleString.charAt(i));
+//
+//        //ex.2
+////        System.out.println("Sum: " + sum(5, 3));
+////        System.out.println("Sum: " + subtract(5, 3));
+////        System.out.println("Sum: " + multi(5, 3));
+////        System.out.println("Sum: " + divide(5, 3));
+////        System.out.println("Sum: " + averageNum(5, 3));
+//
+//        double area = areaOfCircle(2);
+//
+//        System.out.println("area of circle: " + areaOfCircle);
+//
+//
+//    }
+//
+////    public static int sum(int a, int b){
+////        return a+b;
+////    }
+////
+////    public static int subtract(int a, int b){
+////        return a-b;
+////    }
+////
+////    public static int multi(int a, int b){
+////        return a*b;
+////    }
+////
+////    public static int divide(int a, int b){
+////        return a/b;
+////    }
+////
+////    public static int averageNum(int a, int b){
+////        return (a+b)/2;
+////    }
+//
+//    //ex.3
+//
+//    public static int areaOfCircle(double radius) {
+//
+//
+//    }
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter base");
+//        int base=sc.nextInt();
+//        System.out.println("Enter exponent");
+//        int exponent=sc.nextInt();
+//        System.out.println("Power("+base+"^"+exponent+") = "+power(base,exponent));
+//
+//    }
+//
+//    static int power(int b,int e)
+//    {
+//        if(e==0) {
+//            return 1;
+//        }
+//        else {
+//            return (b * power(b, e - 1));
+//        }
+//    }
+
+
+
+}
