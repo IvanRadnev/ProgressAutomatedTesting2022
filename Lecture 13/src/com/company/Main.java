1a7047427112395054326df3bf7d8d525416cd11
IvanRadnev <i.radnev92@gmail.com>
2022-04-14 20:15:13 +0300

    wip

---
+++ Lecture 13/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+
+        int[] numbers = new int[]{3, 5, 8};
+        System.out.print("Enter a number: ");
+        int inputNumber = sc.nextInt();
+
+
+
+    }
+}
